ec256bda3a76ecb01b54e5f6817e4e0b364e476a
Burak <95.burakkara@gmail.com>
2020-02-04 16:59:02 +0300

    4.2.2020

---
+++ Assets/Scripts/lootbox_Opening.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lootbox_Opening : MonoBehaviour
+{
+    public Sprite[] animatedImages;
+    public Image animatedImageObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animatedImageObj.sprite = animatedImages [(int)(Time.time*10) % animatedImages.LongLength];
+    }
+}
